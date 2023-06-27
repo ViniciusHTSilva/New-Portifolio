688cef5fd4d61e4e4d22df32598f1ca3210363c7
Vinicius <88394501+ViniciusHTSilva@users.noreply.github.com>
2023-06-27 00:13:34 -0300

    v 0.1
    primeiros avanços

---
+++ Portifolio/Components/index.jsx
@@ -0,0 +1,24 @@
+
+
+export default function index() {
+    return (
+        <div >
+
+            <div className="init">
+                <h1>
+                    “Se o plano não funcionar, mude o plano, mas nunca o objetivo.”
+
+                </h1>
+
+            </div>
+
+            <div className="button">
+                <button className="sobre">Sobre</button>
+                <button className="projetos">Projetos</button>
+                <button className="contato">Contato</button>
+            </div>
+
+
+        </div>
+    )
+}
---
+++ Portifolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Index from '../Components/index.jsx'
+import '../Components/index.css'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ 
+
+    <Index />
+
+    
+  
+   
+  )
+}
+
+export default App
